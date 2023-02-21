0cf343672ff808155f0b24a6df0b06a65c9157aa
mxs3203 <mxs3203@g.rit.edu>
2023-02-21 21:25:24 +0100

    first commit

---
+++ Snakefile
@@ -0,0 +1,48 @@
+import glob
+import os
+
+from tqdm.asyncio import tqdm
+
+from feature_util import readPickle, savePickle, readAscatSavePickle, computeFeatures, makeFilesForEachSampleAndChr
+
+ruleorder: csv_to_pickle_filter > pickle_to_files > compute_features
+
+rule all:
+    input:
+        "data/output/compute_features/"
+
+rule csv_to_pickle_filter:
+    input:
+        "data/input/hmf_ascat.csv"
+    output:
+        "data/output/csv_to_pickle_filter/hmf_ascat.obj"
+    run:
+        readAscatSavePickle(input[0], output[0])
+
+rule pickle_to_files:
+    input:
+        rules.csv_to_pickle_filter.output
+    output:
+        "data/output/pickle_to_files/"
+    run:
+        df = readPickle(input[0])
+        makeFilesForEachSampleAndChr(df, output[0])
+
+
+rule compute_features:
+    input:
+        rules.pickle_to_files.output
+    output:
+        "data/output/compute_features/"
+    benchmark:
+        "benchmarks/ascatfeatures.csv"
+    run:
+        os.mkdir(output[0])
+        for id in tqdm(glob.glob(input[0] + "/*")): # all sample folder
+            id_folder = "{}/{}/".format(output[0], id.split("/")[3])
+            os.mkdir(id_folder)
+            for chr_df in glob.glob(id + "/*.pickle"):
+                df = readPickle(chr_df)
+                df = computeFeatures(df)
+                file_name = chr_df.split("/")[4]
+                savePickle(df, "{}/{}".format(id_folder, file_name))
---
+++ feature_util.py
@@ -0,0 +1,85 @@
+import os
+
+from tqdm.asyncio import tqdm
+import numpy as np
+np.seterr(divide='ignore')
+import pandas as pd
+import pickle as pk
+
+def readAscatSavePickle(input, output):
+    df = pd.read_csv(input, delimiter=",")
+    print("Before filtering: ",np.shape(df))
+    df = df[~df.Chr.isin([23,24])]
+    df = df.query("ACF < 0.99 and ACF > 0.1")
+    # Do we want to remove 'normal' segments?
+    df = df.query("cn != 2 and Ploidy != 2.0")
+
+    df.loc[df["nAraw"] < 0, "nAraw"] = 0.0
+    df.loc[df["nBraw"] < 0, "nBraw"] = 0.0
+    print("After filtering: ", np.shape(df))
+    with open(output, 'wb') as f:
+        pk.dump(df, f, pk.HIGHEST_PROTOCOL)
+        f.close()
+
+def readPickle(input):
+    return pd.read_pickle(input)
+
+def savePickle(df, output):
+    with open(output, 'wb') as f:
+        pk.dump(df, f, pk.HIGHEST_PROTOCOL)
+        f.close()
+
+def label_loh(row):
+   if row['nA'] == 0 and row['nB'] == 1 and row['cn'] == 1:
+       return 1
+   if row['nA'] == 1 and row['nB'] == 0 and row['cn'] == 1:
+       return 1
+   else:
+       return 0
+
+def makeFilesForEachSampleAndChr(df, out_folder):
+    os.mkdir(out_folder)
+    for id in tqdm(df.ID.unique()):
+        os.mkdir("{}/{}".format(out_folder, id))
+        for c in range(1,23,1):
+            tmp_df = df.loc[df["ID"] == id]
+            tmp_df = tmp_df.loc[df["Chr"] == c]
+            savePickle(tmp_df, "{}/{}/{}.pickle".format(out_folder,id,c))
+
+
+def computeDistance(row):
+    return row
+
+def distanceToClosestCNV(df):
+    n_row = np.shape(df)[0]
+    distance = np.array(df.iloc[range(1, n_row), [3]].values - df.iloc[range(0, n_row - 1), [4]].values)
+    distance = np.insert(distance, 0, 0)
+    df['log10_distanceToNearestCNV'] = np.log10(np.abs(distance+0.0001))
+    return df
+
+
+def changePoint(df):
+    changepoint = df.groupby(['Chr', 'ID'])['logR'].diff().ungroup()
+    print(changepoint[:,0].tolist())
+    return changepoint
+
+def allelicImbalance(row):
+    if row['nA'] != row['nB']:
+        return 1
+    else:
+        return 0
+
+def computeFeatures(df):
+    with np.errstate(all="ignore"):
+        df = distanceToClosestCNV(df)
+        df['logR'] = np.log2(((df['nAraw'] + df['nBraw']) / (df['Ploidy']+0.0001)))
+        df['log10_segmentSize'] = np.log10(df['End'] - df['Start'] + 1)
+        df['loh'] = df.apply(lambda row: label_loh(row), axis=1)
+        df['allelicImbalance'] = df.apply(lambda row: allelicImbalance(row), axis=1)
+    #df['changePointCN'] = changePoint(df)
+    #print(df.head(5))
+
+    return df
+
+def changePoint(df):
+    return df
